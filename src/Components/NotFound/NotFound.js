6d63e0f9d92bd3d912d460d5cb76fa1914c252aa
owadud <akashahmed104466@gmail.com>
2022-04-05 22:29:28 +0600

    Complete

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './NotFound.css';
 const NotFound = () => {
     return (
-        <div>
+        <div className="NotFound">
             <h1>Sorry! The page you are trying to access is not available</h1>
         </div>
     );
